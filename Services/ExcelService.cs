cb946b8d6d25aa8c140e20b18a99b7bd624c7ae7
SHthemW <2536076378@qq.com>
2023-10-09 19:13:27 +0800

    [ dev ][ temp ] excel service

---
+++ Services/ExcelService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI;
+using NPOI.SS.UserModel;
+
+namespace UxGame_Testing_Utility.Services
+{
+    internal sealed class ExcelService
+    {
+        private readonly string _excelFilePath;
+
+        // TODO: finished
+    }
+}
