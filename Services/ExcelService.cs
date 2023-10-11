d6325ebafb5b3d57b5860a844f4aed3e8bd342c7
SHthemW <2536076378@qq.com>
2023-10-11 16:53:10 +0800

    [ opt ] 优化ExcelService中的程序结构.

--- Services/ExcelService.cs
+++ Services/ExcelService.cs
@@ -8,7 +8,11 @@ namespace UxGame_Testing_Utility.Services
 {
     internal sealed class ExcelService
     {
-        private readonly IWorkbook? _excelFile;
+        private readonly ISheet? _excelSheet;
+
+        private readonly int _skillIdColumnIndex;
+        private readonly int _buletIdColumnIndex;
+        private readonly int _shotrIdColumnIndex;
 
         private const string DATA_SHEET_NAME = "Data";
         private const string SKILLID_COLNAME = "Id";
@@ -18,6 +22,8 @@ namespace UxGame_Testing_Utility.Services
         internal ExcelService(string excelFilePath, out string? errmsg)
         {
             errmsg = default;
+
+            #region Init Excel File
             try
             {
                 using FileStream file = new(
@@ -25,57 +31,53 @@ namespace UxGame_Testing_Utility.Services
                 FileMode.Open,
                 FileAccess.ReadWrite
                 );
-                _excelFile = new XSSFWorkbook(file);
+                _excelSheet = new XSSFWorkbook(file).GetSheet(DATA_SHEET_NAME);
             }
             catch (IOException)
             {
                 errmsg = "file is in occupying, please close and retry.";
+                return;
             }
+            #endregion
+
+            #region Init Basic Properties
+            try
+            {
+                _skillIdColumnIndex = TryGetColumnIndex(SKILLID_COLNAME);
+                _buletIdColumnIndex = TryGetColumnIndex(BULETID_COLNAME);
+                _shotrIdColumnIndex = TryGetColumnIndex(SHOTRID_COLNAME);
+            }
+            catch (NullReferenceException)
+            {
+                errmsg = "excel sheet is failed to init.";
+                return;
+            }
+            catch (KeyNotFoundException e)
+            {
+                errmsg = $"name {e.Message} cannot be found in file.";
+                return;
+            }
+            #endregion 
         }
         internal bool GetSkillGroup(string targetId, out SkillGroup group, out string? errmsg)
         {
-            #region Init Table & Properties
-
-            var sheet = _excelFile!.GetSheet(DATA_SHEET_NAME);       
-            
-            if (!TryGetColumnIndex(SKILLID_COLNAME, sheet, out int skillIdColIndex))
-            {
-                group = default;
-                errmsg = $"column <{SKILLID_COLNAME}> was not found in sheet";
-                return false;
-            }
-            if (!TryGetColumnIndex(BULETID_COLNAME, sheet, out int bulletIdColIndex))
-            {
-                group = default;
-                errmsg = $"column <{BULETID_COLNAME}> was not found in sheet";
-                return false;
-            }
-            if (!TryGetColumnIndex(SHOTRID_COLNAME, sheet, out int shooterIdIndex))
-            {
-                group = default;
-                errmsg = $"column <{SHOTRID_COLNAME}> was not found in sheet";
-                return false;
-            }
-
-            #endregion
-
             List<Skill> skillsInGroup = new();
-            for (int i = 1; i <= sheet.LastRowNum; i++)
+            for (int i = 1; i <= _excelSheet!.LastRowNum; i++)
             {
-                IRow row = sheet.GetRow(i);
+                IRow row = _excelSheet.GetRow(i);
                 if (row == null)
                     continue;
 
-                var currentId = row.GetCell(skillIdColIndex)?.ToString();
+                var currentId = row.GetCell(_skillIdColumnIndex)?.ToString();
                 if (string.IsNullOrEmpty(currentId))
                     continue;
 
                 if (Skill.IsSame(currentId, targetId))
                 {
                     skillsInGroup.Add(new Skill(
-                        Id:        currentId, 
-                        BulletId:  row.GetCell(bulletIdColIndex).ToString()!, 
-                        ShooterId: row.GetCell(shooterIdIndex).ToString()!
+                        Id: currentId,
+                        BulletId: row.GetCell(_buletIdColumnIndex).ToString()!,
+                        ShooterId: row.GetCell(_shotrIdColumnIndex).ToString()!
                         ));
                 }
             }
@@ -95,35 +97,23 @@ namespace UxGame_Testing_Utility.Services
         }
         internal bool ApplySkillGroupDataOn(SkillGroup skillGroup, int rowIndex, out string? errmsg)
         {
-            var sheet = _excelFile!.GetSheet(DATA_SHEET_NAME);
-
-            if (!TryGetColumnIndex(SKILLID_COLNAME, sheet, out int skillIdColIndex))
-            {
-                errmsg = $"column <{SKILLID_COLNAME}> was not found in sheet";
-                return false;
-            }
-            if (!TryGetColumnIndex(BULETID_COLNAME, sheet, out int bulletIdColIndex))
-            {
-                errmsg = $"column <{BULETID_COLNAME}> was not found in sheet";
-                return false;
-            }
-            if (!TryGetColumnIndex(SHOTRID_COLNAME, sheet, out int shooterIdColIndex))
-            {
-                errmsg = $"column <{SHOTRID_COLNAME}> was not found in sheet";
-                return false;
-            }
-
-            var testAreaSkillId = sheet.GetRow(rowIndex).GetCell(skillIdColIndex).ToString();
+            var testAreaSkillId = _excelSheet!.GetRow(rowIndex).GetCell(_skillIdColumnIndex).ToString();
 
             foreach (var data in skillGroup.Skills)
             {
-                if (sheet.GetRow(rowIndex).GetCell(skillIdColIndex).ToString() != testAreaSkillId )
+                var currentAreaId = _excelSheet.GetRow(rowIndex).GetCell(_skillIdColumnIndex).ToString();
+                if (!Skill.IsSame(currentAreaId!, testAreaSkillId!))
                 {
-                    errmsg = "test case writing overflow, please check the lv count of testskill.";
+                    errmsg = 
+                        $"test case writing overflow: " +
+                        $"test area only in id {testAreaSkillId}, " +
+                        $"but now is flushing in {currentAreaId}. " +
+                        $"please check the lv count of testskill.";
                     return false;
                 }
-                sheet.GetRow(rowIndex).CreateCell(bulletIdColIndex).SetCellValue(data.BulletId);
-                sheet.GetRow(rowIndex).CreateCell(shooterIdColIndex).SetCellValue(data.ShooterId);
+
+                _excelSheet.GetRow(rowIndex).CreateCell(_buletIdColumnIndex).SetCellValue(data.BulletId);
+                _excelSheet.GetRow(rowIndex).CreateCell(_shotrIdColumnIndex).SetCellValue(data.ShooterId);
                 rowIndex++;
             }
 
@@ -133,22 +123,19 @@ namespace UxGame_Testing_Utility.Services
 
         private ExcelService()
         {
-            _excelFile = default;
+            _excelSheet = default;
         }
-        private static bool TryGetColumnIndex(string columnName, ISheet sheet, out int result)
+        private int TryGetColumnIndex(string columnName)
         {
-            var headerRow = sheet.GetRow(0);
+            if (_excelSheet == null)
+                throw new NullReferenceException();
 
+            var headerRow = _excelSheet.GetRow(0);
             for (int i = 0; i < headerRow.LastCellNum; i++)
-            {
                 if (headerRow.GetCell(i).ToString() == columnName)
-                {
-                    result = i;
-                    return true;
-                }
-            }
-            result = -1;
-            return false;
+                    return i;
+            
+            throw new KeyNotFoundException(columnName);
         }
     }
 }
